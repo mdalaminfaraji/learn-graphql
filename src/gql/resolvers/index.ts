9a9fcd5f0a919b3096ea38dde6eb88d9d1ca5ed2
alaminfaraji <alaminice1617@gmail.com>
2024-06-04 09:52:01 +0600

    basic graphql

---
+++ src/gql/resolvers/index.ts
@@ -0,0 +1,35 @@
+import { db } from "../../db.js";
+
+export const resolvers = {
+  Query: {
+    products: () => db.products,
+    product: (parent: any, args: { productId: String }, context: any) => {
+      const result = db.products.find((pd) => pd.id === args.productId);
+      return result;
+    },
+    categories: () => db.categories,
+    category: (parent: any, args: { categoryId: String }, context: any) => {
+      const result = db.categories.find(
+        (category) => category.id === args.categoryId
+      );
+      return result;
+    },
+  },
+
+  Product: {
+    category: (parent, args, context) => {
+      return db.categories.find(
+        (category) => category.id === parent.categoryId
+      );
+    },
+    reviews: (parent, args, context) => {
+      return db.reviews.filter((review) => review.productId === parent.id);
+    },
+  },
+
+  Category: {
+    products: (parent, args, context) => {
+      return db.products.filter((product) => product.categoryId === parent.id);
+    },
+  },
+};
